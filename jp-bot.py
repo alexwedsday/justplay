892d708738cf66f337ead5ca167a9ec77551f618
Alex <alexwedsday@hotmail.com>
2026-02-04 05:39:26 +0000

    fixed

--- jp-bot.py
+++ jp-bot.py
@@ -49,6 +49,7 @@ async def play_url(message, url):
     
     ydl_opts = {
     'format': 'm4a/bestaudio/best',
+    'cookieFile': 'cookies.txt',
     'postprocessors': [{ 
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'm4a',
